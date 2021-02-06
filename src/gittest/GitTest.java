6b64f816a5b90523b4eeee7247fb4b36b5f4a6e0
Juan Camilo Villa <juanc.villa@upb.edu.co>
2021-02-06 12:24:46 -0500

    Cracion del proyecto en el repositoro -000329745

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,16 @@
+package gittest;
+
+/**
+ *
+ * @author 000329745 Juan Camilo Villa P
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+    
+}
